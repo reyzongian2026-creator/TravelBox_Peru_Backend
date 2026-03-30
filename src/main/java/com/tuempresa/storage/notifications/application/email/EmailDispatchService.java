0d63aa142274ce0b47209f9f828a9932178a5865
GianLH <reyzongian2026@icloud.com>
2026-03-30 13:43:40 -0500

    fix: omit graph from field for app mail

--- src/main/java/com/tuempresa/storage/notifications/application/email/EmailDispatchService.java
+++ src/main/java/com/tuempresa/storage/notifications/application/email/EmailDispatchService.java
@@ -189,16 +189,6 @@ public class EmailDispatchService {
         String sendMailUrl = String.format(GRAPH_SEND_MAIL_URI_TEMPLATE, URLEncoder.encode(senderEmail, StandardCharsets.UTF_8));
 
         Map<String, Object> message = new LinkedHashMap<>();
-        message.put(
-                "from",
-                Map.of(
-                        "emailAddress",
-                        Map.of(
-                                "address", senderEmail,
-                                "name", fromName != null ? fromName : "InkaVoy"
-                        )
-                )
-        );
         message.put("toRecipients", List.of(Map.of("emailAddress", Map.of("address", recipient))));
         message.put("subject", subject);
 
